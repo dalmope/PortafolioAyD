52a648ee512964b1291ec1993528dc5e042b3d2b
David Mora <daamora@bancolombia.com.co>
2023-11-17 08:19:13 -0500

    deploy try

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: 'https://astroship.web3templates.com',
   integrations: [
     tailwind(),
     mdx(),
