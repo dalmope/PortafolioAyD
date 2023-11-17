a69c7b4fcaf93457e9eee3bba231b6ca830dc510
David Mora <daamora@bancolombia.com.co>
2023-11-17 09:38:18 -0500

    ajustes para nuevo blog

--- src/content/config.ts
+++ src/content/config.ts
@@ -34,6 +34,7 @@ const teamCollection = defineCollection({
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  'blog': blogCollection,
+  'blogAnalisis': blogCollection,
+  'blogIngenieria': blogCollection,
   'team': teamCollection,
 };
